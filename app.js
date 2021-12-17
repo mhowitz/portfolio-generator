01ff2a48626983f873b4cbb00e6c226e024aea84
mikwitz <mikaylahowitz@gmail.com>
2021-12-17 13:30:12 -0700

    beggining inquirer

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const name1 = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//below does same thing
-const [name, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHMTL = generatePage(name, github);
+
+const inquirer = require('inquirer');
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => {
+        console.log(JSON.stringify(answers));
+    });
 
-    console.log('Porftolio Complete! Check out index.html to see the output!');
-})
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Porftolio Complete! Check out index.html to see the output!');
+// })
